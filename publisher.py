09cf3391eedbc1adabb35663a8ffaea1008f2128
philipp <philipp3923@icloud.com>
2024-04-24 17:19:23 +0200

    fix black

--- publisher.py
+++ publisher.py
@@ -9,17 +9,23 @@ broker_address = "broker.hivemq.com"
 topic = "DataMgmt"
 
 # Create an MQTT client
-client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, "piuabsdfuwaeg4wq98thqb3pfdgdfhg43gq8b9ur0vb97", clean_session=False, )
+client = mqtt.Client(
+    mqtt.CallbackAPIVersion.VERSION2,
+    "piuabsdfuwaeg4wq98thqb3pfdgdfhg43gq8b9ur0vb97",
+    clean_session=False,
+)
 client.loop_start()
 
 # Connect to the broker
 client.connect(broker_address, 1883, 60)
 
-message = json.dumps({
-    "fin":"FEFEFEFEFEFEF6969",
-    "zeit":int(time.time()),
-    "geschwindigkeit":int(np.random.rand()*50)
-})
+message = json.dumps(
+    {
+        "fin": "FEFEFEFEFEFEF6969",
+        "zeit": int(time.time()),
+        "geschwindigkeit": int(np.random.rand() * 50),
+    }
+)
 
 # Publish a message to the topic (json)
 while True:
--- subscriber.py
+++ subscriber.py
@@ -7,11 +7,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-conn = psycopg2.connect(dbname="postgres", user="postgres", password=os.environ["POSTGRES_PASSWORD"])
+conn = psycopg2.connect(
+    dbname="postgres", user="postgres", password=os.environ["POSTGRES_PASSWORD"]
+)
 cur = conn.cursor()
 
 # create table
-cur.execute("""    
+cur.execute(
+    """    
     create schema if not exists staging;
     
     create table if not exists staging.messung (
@@ -20,22 +23,30 @@ cur.execute("""
         empfangen TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP, 
         CONSTRAINT pk_messung PRIMARY KEY(messung_id)
     );
-""")
+"""
+)
 conn.commit()
 
 # Define the broker and topic
 broker_address = "broker.hivemq.com"
 topic = "DataMgmt"
 
+
 # Callback when a message is received from the broker
 def on_message(client, userdata, msg):
     print(f"Received message: {msg.payload.decode()} on topic {msg.topic}")
-    cur.execute("insert into staging.messung (payload) values (%s)", (msg.payload.decode(),))
+    cur.execute(
+        "insert into staging.messung (payload) values (%s)", (msg.payload.decode(),)
+    )
     conn.commit()
 
 
 # Create an MQTT client
-client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, "piuabsdfuwaeg4wq98thqb3p4bg43gq8b9ur0vb97", clean_session=False, )
+client = mqtt.Client(
+    mqtt.CallbackAPIVersion.VERSION2,
+    "piuabsdfuwaeg4wq98thqb3p4bg43gq8b9ur0vb97",
+    clean_session=False,
+)
 
 # Set the message callback
 client.on_message = on_message
